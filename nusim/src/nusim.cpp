ed6b2dd18487bc91ef1fd917d2348a495b311b0f
Marco Morales <marcomorales2022@u.northwestern.edu>
2022-01-09 12:42:17 -0600

    Launchfile added node and edited node to increment timestep

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -1,29 +1,53 @@
 #include"ros/ros.h"
 #include"std_msgs/UInt64.h"
+#include <iostream>
+#include <ros/console.h>
 
 class Sim
 {
     public:
-        Sim():
-            nh{},
-            pub(nh.advertise<std_msgs::UInt64>("js", 5)),
-            timer(nh.createTimer(ros::Duration(0.002), &Sim::main_loop, this))
-            // nh().getParam("private_name", param)
-         {
-         }
+        Sim() {
+            data_val = 0;
+            timestep.data = 0;
+            nh.getParam("/nusim/rate",rate);
+            pub = nh.advertise<std_msgs::UInt64>("UInt64", 100);
+            ros::ServiceServer nh.advertiseService(const std::string& service, reset);
 
+            
+            main_loop();
+            // while(ros::ok()) {
+            //     timestep.data++;
+            //     timer = nh.createTimer(ros::Duration(1/rate), &Sim::main_loop, this);
+            // }
 
-         void main_loop(const ros::TimerEvent &) const
-         {
-            // implement the state machine here
-             pub.publish(std_msgs::UInt64{});
-         }
+    
+        }
+           
+        //  void main_loop(const ros::TimerEvent &) const
+        //  {
+        //     // implement the state machine here
+        //     // data_val = data_val +1;
+        //     // std::cout << data_val << "\n";
+        //     pub.publish(timestep);
+        //     //  ROS_INFO("Hello ");
+        //  }
+        void main_loop() {
+            ros::Rate new_rate(rate);
+            while(ros::ok()) {
+                timestep.data++;
+                pub.publish(timestep);
+                new_rate.sleep();
+            }
+        }
 
     private:
         ros::NodeHandle nh;
         ros::Publisher pub;
         ros::Timer timer;
-        // std::string param;
+        double rate;
+        std_msgs::UInt64 timestep;
+        int data_val;
+
 
 };
 
