49f1d29bfe3e3efe43b95c369c79e2d7c829c169
Marco Morales <marcomorales2022@u.northwestern.edu>
2022-02-11 18:08:17 -0600

    commented test

--- nuturtle_control/test/turtle_interface_test.cpp
+++ nuturtle_control/test/turtle_interface_test.cpp
@@ -5,10 +5,11 @@
 #include <nuturtlebot_msgs/SensorData.h>
 #include <sensor_msgs/JointState.h>
 
+//global variables
 geometry_msgs::Twist cmd_vel;
 nuturtlebot_msgs::WheelCommands wheel_cmd;
 sensor_msgs::JointState jointStates;
-
+// subscriber callbacks
 void wheel_cmd_callback(const nuturtlebot_msgs::WheelCommands & msg)
 {
   wheel_cmd = msg;
@@ -23,15 +24,15 @@ void js_callback(const sensor_msgs::JointState & js)
 
 TEST_CASE("Purely Translational Wheel_cmds", "[nuturtle_control]") {
   int i;
-  ros::NodeHandle nh; // this initializes time
-
+  ros::NodeHandle nh; 
+  // init publishers and subscribers
   ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel",1);
   ros::Subscriber js_sub = nh.subscribe("/wheel_cmd", 1, wheel_cmd_callback);
-
+  // create twist
   geometry_msgs::Twist twist;
   twist.linear.x = 0.1;
   twist.angular.z = 0.1;
-
+  // run for 2 seconds
   ros::Rate r(500);
   while(1){
     i++;
@@ -49,15 +50,14 @@ TEST_CASE("Purely Translational Wheel_cmds", "[nuturtle_control]") {
 
 TEST_CASE("Purely Rotational Wheel_cmds", "[nuturtle_control]") {
   int i;
-  ros::NodeHandle nh; // this initializes time
+  ros::NodeHandle nh; 
 
   ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel",1);
   ros::Subscriber wheel_cmd_sub = nh.subscribe("/wheel_cmd", 1, wheel_cmd_callback);
-
+  // create twist
   geometry_msgs::Twist twist;
-  // twist.linear.x = 0.1;
   twist.angular.z = 0.1;
-
+  // run for 2 seconds
   ros::Rate r(500);
   while(1){
     i++;
@@ -74,15 +74,15 @@ TEST_CASE("Purely Rotational Wheel_cmds", "[nuturtle_control]") {
 
 TEST_CASE("Sensor Encoder data", "[nuturtle_control]") {
   int i;
-  ros::NodeHandle nh; // this initializes time
+  ros::NodeHandle nh;
 
   ros::Publisher sense_pub = nh.advertise<nuturtlebot_msgs::SensorData>("/sensor_data",1);
   ros::Subscriber wheel_cmd_sub = nh.subscribe("/joint_states", 1, js_callback);
-
+  // create sensorData data type variable
   nuturtlebot_msgs::SensorData sense_data;
   sense_data.left_encoder = 10;
   sense_data.right_encoder = 10;
-
+  // run for 1 second
   ros::Rate r(500);
   while(1){
     i++;
